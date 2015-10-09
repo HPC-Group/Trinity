d637a82ffa8a881e811bf92b6bc98cbb87087bec
Andrey Krekhov <andrey.krekhov@uni-due.de>
2015-10-09 13:00:37 +0000

    added clean cmake and gtest structure

---
+++ src/tests/main.cpp
@@ -0,0 +1,19 @@
+#include "gtest/gtest.h"
+
+#include "mocca/log/LogManager.h"
+#include "mocca/log/ConsoleLog.h"
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+
+  using mocca::LogManager;
+#ifdef MUI_TEST_LOGGING
+  LogManager::initialize(LogManager::LogLevel::Debug, true);
+#else
+  LogManager::initialize(LogManager::LogLevel::None, true);
+#endif
+  auto log = new mocca::ConsoleLog();
+  LogMgr.addLog(log);
+
+  return RUN_ALL_TESTS();
+}
