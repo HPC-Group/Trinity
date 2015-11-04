492408a2a5204fc0e0c2dc59435147f6d6eb3e52
Andrey Krekhov <andrey.krekhov@uni-due.de>
2015-11-04 15:26:09 +0000

    added three dummy node projects

---
+++ branches/vaas/src/frontend-dummy/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+  
+  std::cout << "frontend dummy node" << std::endl;
+  
+  return 0;
+}
---
+++ branches/vaas/src/io-dummy/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+  
+  std::cout << "IO dummy node" << std::endl;
+  
+  return 0;
+}
