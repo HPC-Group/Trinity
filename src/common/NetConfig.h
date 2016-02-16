19b06e2d6647eb8b8bfa3c18c2f1a685a41f9023
Andrey Krekhov <andrey.krekhov@uni-due.de>
2016-02-16 09:53:58 +0000

    increased reply-timeout in channel

--- src/common/NetConfig.h
+++ src/common/NetConfig.h
@@ -4,7 +4,7 @@
 namespace trinity {
 namespace common {
     
-static const std::chrono::milliseconds TIMEOUT_REPLY(500);
+static const std::chrono::milliseconds TIMEOUT_REPLY(1000);
 static const int RENDERSESSION_BASE_PORT = 5999;
 static const int VISRECEIVER_BASE_PORT = 6999;
 }
