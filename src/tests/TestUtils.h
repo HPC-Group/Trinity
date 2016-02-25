3c06b20f49cbb871d929ede8f67d709ae3dc27cf
David McCann <mccann.dmc@gmail.com>
2016-02-25 12:19:02 +0000

    added missing namespace

--- src/tests/TestUtils.h
+++ src/tests/TestUtils.h
@@ -7,11 +7,11 @@ namespace trinity {
 namespace testing {
 
 template <typename T> T writeAndRead(const T& obj) {
-    auto writer = ISerializerFactory::defaultFactory().createWriter();
+    auto writer = commands::ISerializerFactory::defaultFactory().createWriter();
     obj.serialize(*writer);
     T result;
     auto serialized = writer->write();
-    auto reader = ISerializerFactory::defaultFactory().createReader(serialized);
+    auto reader = commands::ISerializerFactory::defaultFactory().createReader(serialized);
     result.deserialize(*reader);
     return result;
 }
