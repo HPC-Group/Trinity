a1bce5c5519eb26f2019038b139a47415b312c9e
David McCann <mccann.dmc@gmail.com>
2016-02-27 15:16:42 +0000

    trying to fix mac build

--- src/commands/JsonWriter.cpp
+++ src/commands/JsonWriter.cpp
@@ -24,19 +24,19 @@ void JsonWriter::append(const std::string& key, const ISerializable& obj) {
 
 void JsonWriter::append(const std::string& key, const std::vector<float>& vec) {
     for (size_t i = 0; i < vec.size(); ++i) {
-        m_root[key][i] = vec[i];
+        m_root[key][i] = JsonCpp::Value(vec[i]);
     }
 }
 
 void JsonWriter::append(const std::string& key, const std::vector<int>& vec) {
     for (size_t i = 0; i < vec.size(); ++i) {
-        m_root[key][i] = vec[i];
+        m_root[key][i] = JsonCpp::Value(vec[i]);
     }
 }
 
 void JsonWriter::append(const std::string& key, const std::vector<std::string>& vec) {
     for (size_t i = 0; i < vec.size(); ++i) {
-        m_root[key][i] = vec[i];
+        m_root[key][i] = JsonCpp::Value(vec[i]);
     }
 }
 
