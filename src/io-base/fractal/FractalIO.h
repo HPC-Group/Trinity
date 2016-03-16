92ede020f98a5d950a3c6ee99cae9b4f5cbe04f2
Jens Krüger <jens.krueger@uni-due.de>
2016-03-16 22:06:55 +0000

    fractal IO, full of ugly hacks

--- src/io-base/fractal/FractalIO.h
+++ src/io-base/fractal/FractalIO.h
@@ -4,47 +4,57 @@
 #include "common/IIO.h"
 #include "mocca/log/LogManager.h"
 
-/**
- OLD FILE - modified to fit the IIO
- */
-
 namespace trinity {
+  
+  class FractalIO : public IIO {
+  public:
+    FractalIO();
+    FractalIO(int fileId);
+    
+    
+    Core::Math::Vec3ui64 getMaxBrickSize() const override;
+    Core::Math::Vec3ui64 getMaxUsedBrickSizes() const override;
+    MinMaxBlock maxMinForKey(const BrickKey&) const override;
+    int getLODLevelCount() const override;
+    uint64_t getNumberOfTimesteps() const override;
+    Core::Math::Vec3ui64 getDomainSize(uint64_t lod, uint64_t modality,
+                                       uint64_t ts) const override;
+    Core::Math::Mat4d getTransformation(uint64_t modality) const override;
+    Core::Math::Vec3ui getBrickOverlapSize() const override;
+    uint64_t getLargestSingleBrickLOD(uint64_t modality,
+                                      uint64_t ts) const override;
+    Core::Math::Vec3ui getBrickVoxelCounts(const BrickKey&) const override;
+    Core::Math::Vec3f getBrickExtents(const BrickKey &) const override;
+    Core::Math::Vec3ui getBrickLayout(uint64_t lod, uint64_t modality,
+                                      uint64_t timestep) const override;
+    uint64_t getModalityCount() const override;
+    uint64_t getComponentCount(uint64_t modality) const override;
+    Core::Math::Vec2f getRange(uint64_t modality) const override;
+    uint64_t getTotalBrickCount() const override;
+    bool getBrick(const BrickKey& brickKey,
+                  std::vector<uint8_t>& data) const; // HACK: override;
 
-class FractalIO : public IIO {
-public:
-    FractalIO() { LINFO("(io) fractal io created"); }
-    FractalIO(int fileId) { LINFO("(io) fractal io created"); }
-    // testing purpose
-
-
-    /*
-    FractalIO(uint64_t sx, uint64_t sy, uint64_t sz,
-              uint64_t bx, uint64_t by, uint64_t bz);
-     */
-
-    Core::Math::Vec3ui64 getMaxBrickSize() const override {Core::Math::Vec3ui64 x; return x; }
-    Core::Math::Vec3ui64 getMaxUsedBrickSizes() const override {Core::Math::Vec3ui64 x; return x; }
-    MinMaxBlock maxMinForKey(const BrickKey&) const override {MinMaxBlock x; return x; }
-    int getLODLevelCount() const override { return 44; }
-    uint64_t getNumberOfTimesteps() const override {uint64_t x; return x; }
-    Core::Math::Vec3ui64 getDomainSize(uint64_t lod, uint64_t modality, uint64_t ts) const override {Core::Math::Vec3ui64 x; return x; }
-    Core::Math::Mat4d getTransformation(uint64_t modality) const override {Core::Math::Mat4d x; return x; }
-    Core::Math::Vec3ui getBrickOverlapSize() const override {Core::Math::Vec3ui x; return x; }
-    uint64_t getLargestSingleBrickLOD(uint64_t modality, uint64_t ts) const override {uint64_t x; return x; }
-    Core::Math::Vec3ui getBrickVoxelCounts(const BrickKey&) const override {Core::Math::Vec3ui x; return x; }
-    Core::Math::Vec3f getBrickExtents(const BrickKey &) const override {Vec3f x; return x; }
-    Core::Math::Vec3ui getBrickLayout(uint64_t lod, uint64_t modality, uint64_t timestep) const override {Core::Math::Vec3ui x; return x; }
-    uint64_t getModalityCount() const override {uint64_t x; return x; }
-    uint64_t getComponentCount(uint64_t modality) const override {uint64_t x; return x; }
-    Core::Math::Vec2f getRange(uint64_t modality) const override {Core::Math::Vec2f x; return x; }
-    uint64_t getTotalBrickCount() const override {uint64_t x; return x; }
-    std::vector<uint8_t> getBrick(const BrickKey& brickKey, bool&) const override { return std::vector<uint8_t>(); }
-
-private:
+    // HACK
+    std::vector<uint8_t> getBrick(const BrickKey& brickKey,
+                                  bool& success) const override {}
+    
+    
+  private:
     Core::Math::Vec3ui64 m_totalSize;
     Core::Math::Vec3ui64 m_brickSize;
     bool m_bFlat;
-    // Mandelbulb<uint8_t> m_mbGenerator;
+    Mandelbulb<uint8_t> m_mbGenerator;
     
-};
+    // HACK: that's what I would like todo but the API won't let me so I had to
+    // move the constructor into the pseudo constructor below
+    /*
+     FractalIO(uint64_t sx, uint64_t sy, uint64_t sz,
+     uint64_t bx, uint64_t by, uint64_t bz);
+     */
+    
+    void pseudoConstructor(uint64_t sx, uint64_t sy, uint64_t sz,
+                           uint64_t bx, uint64_t by, uint64_t bz);
+    
+    
+  };
 }
