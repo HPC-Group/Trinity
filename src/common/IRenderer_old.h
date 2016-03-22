cf5e6bd9819a797e74997d5b306533a7e80895a1
Jens Krüger <jens.krueger@uni-due.de>
2016-03-22 14:14:43 +0000

    new API

--- src/common/IRenderer_old.h
+++ src/common/IRenderer_old.h
@@ -1,104 +1,97 @@
-//
-//  IRenderer.h
-//  TNG
-
-#ifndef TNG_IRenderer_h
-#define TNG_IRenderer_h
-
-
-//#include "Service/RenderState.h"
-#include <renderer/Context/Context.h>
-
-#include <thread>
-
-namespace Tuvok {
-class IOLocal;
-typedef std::shared_ptr<IOLocal> IOPtr;
-
-namespace Renderer {
-
-struct FrameData {
-    FrameData(uint64_t frame, std::vector<uint8_t> data)
-        : _frameID(frame)
-        , _data(data){};
-    FrameData()
-        : _frameID(0)
-        , _data(){};
-
-    uint64_t _frameID;
-    std::vector<uint8_t> _data; // raw image data
-    double _sendTime;
+enum ERenderMode {
+  RM_1DTRANS = 0,
+  RM_2DTRANS,
+  RM_ISOSURFACE,
+  RM_CLEARVIEW,
+  RM_INVALID
 };
 
-class IRenderer {
-public:
-    IRenderer(){};
-    virtual ~IRenderer(){};
+enum BBoxMode {
+  BBM_NONE = 0,
+  BBM_DATASET,
+  BBM_BRICKS
+};
 
 
-    // BASIC INTERACTION METHODS
-
-    // Camera Controls
-    virtual void setFirstPersonMode(bool mode) = 0;
-    virtual void rotateCamera(Core::Math::Vec3f rotation) = 0;
-    virtual void moveCamera(Core::Math::Vec3f direction) = 0;
-    // virtual void SetCamera(Camera c) = 0;
-    virtual void setCameraZoom(float f) = 0;
-    virtual void zoomCamera(float f) = 0;
-    virtual void resetCamera() = 0;
-
-    // ISO FUNCTIONS
-    virtual void setIsoValue(float fIsoValue) = 0;
-    virtual void setIsoValueRelative(float fIsovalue) = 0;
-    virtual float getIsoValue() = 0;
-    virtual void setIsosurfaceColor(const Core::Math::Vec3f& vColor) = 0;
-    virtual Core::Math::Vec3f getIsosurfaceColor() const = 0;
-    // END ISO FUNCTIONS
+struct phongColorTripple {
+  Core::Math::Vec4ui8 ambient;
+  Core::Math::Vec4ui8 diffuse;
+  Core::Math::Vec4ui8 specular;
+};
 
 
-    virtual void setViewParameters(float angle, float znear, float zfar) = 0;
-    virtual void setBackgroundColors(std::pair<Core::Math::Vec3f, Core::Math::Vec3f> colors) = 0;
-    virtual std::pair<Core::Math::Vec3f, Core::Math::Vec3f> getBackgroundColors(const uint8_t index) = 0;
 
-    virtual void setUseLighting(bool bUseLighting) = 0;
-    virtual void setLightingParameters(Core::Math::Vec4f cAmbient, Core::Math::Vec4f cDiffuse, Core::Math::Vec4f cSpecular) = 0;
-    virtual const Core::Math::Vec4f getAmbientColor() = 0;
-    virtual const Core::Math::Vec4f getDiffuseColor() = 0;
-    virtual const Core::Math::Vec4f getSpecularColor() = 0;
+// GLOBAL RENDERMODE SETTINGS
+virtual void setRenderMode(ERenderMode mode) = 0;
+virtual bool supportsRenderMode(ERenderMode mode) = 0;
 
-    virtual void setSampleRateModifier(float fSampleRateModifier) = 0;
-    virtual float getSampleRateModifier() = 0;
+virtual void setModality(uint64_t modality) const = 0;
+virtual void uint64_t getModality() const = 0;
+virtual void setTimestep(uint64_t timestep) const = 0;
+virtual void uint64_t getTimestep() const = 0;
+
+// 1D TF
+virtual void set1DTransferFunction(const TransferFunction1D& tf) = 0;
+virtual const TransferFunction1D getDefault1DTransferFunction(uint64_t index) const = 0;
+virtual const uint64_t getDefault1DTransferFunctionCount() const = 0;
+virtual std::vector<uint64_t> get1DHistogram() const = 0;
+
+// 2D TF
+virtual void set2DTransferFunction(const TransferFunction2D& tf) = 0;
+virtual const TransferFunction2D getDefaul2DTransferFunction(uint64_t index) const = 0;
+virtual const uint64_t getDefault2DTransferFunctionCount() const = 0;
+virtual std::vector<uint64_t> get2DHistogram() const = 0;
+
+// ISO-SURFACE FUNCTIONS
+virtual void setIsoValue(uint8_t surfaceIndex, float fIsoValue) = 0;
+virtual float getIsoValue(uint8_t surfaceIndex) = 0;
+virtual void setIsosurfaceColor(uint8_t surfaceIndex, const Core::Math::Vec3ui8& vColor) = 0;
+virtual Core::Math::Vec3ui8 getIsosurfaceColor(uint8_t surfaceIndex) const = 0;
+virtual Core::Math::Vec2f getRange() const = 0;
+
+// CLEARVIEW FUNCTIONS
+virtual void setClearViewPosition(const Core::Math::Vec2f& vNormalizedWindowPos) = 0;
+virtual void setClearViewRadius(float f) = 0;
+// isovalues and colors are set using the iso functions with surface index 1
+
+// LIGTHING FUNCTIONS
+virtual void enableLighting(bool enable) = 0;
+virtual void setLightingColors(const phongColorTripple& colors) = 0;
+virtual const phongColorTripple getLightingColors() const = 0;
+virtual void setLightDirection(const Core::Math::Vec3f& direction) = 0;
+virtual Core::Math::Vec3f getLightDirection() const = 0;
+
+// SAMPLE RATE FUNCTIONS
+virtual void setSampleRateModifier(float fSampleRateModifier) = 0;
+virtual float getSampleRateModifier() = 0;
 
 
-    virtual Core::Math::Vec2ui getSize() const = 0;
-    virtual void setSize(const Core::Math::Vec2ui& vWinSize) = 0;
+// render parameters
+virtual void setBoundingBoxMode(BBoxMode mode) = 0;
+virtual BBoxMode getBoundingBoxMode() const = 0;
 
-    virtual void getVolumeAABB(Core::Math::Vec3f& vCenter, Core::Math::Vec3f& vExtend) const = 0;
+virtual void setRendererSpecials(const std::vector<uint64_t>& params) = 0;
 
-    // implement in subclasses
-    virtual void setViewPort(Core::Math::Vec2ui lower_left, Core::Math::Vec2ui upper_right,
-                             bool decrease_screen_res = false) = 0; // glrenderer
+virtual void setBackgroundColors(std::pair<Core::Math::Vec3ui8, Core::Math::Vec3ui8> colors) = 0;
+virtual std::pair<Core::Math::Vec3ui8, Core::Math::Vec3ui8> getBackgroundColors() = 0;
 
-    virtual void SetLoDFactor(const float f) = 0;
+// CLIPPING
+virtual void setClipVolume(Core::Math::Vec3f minValues, Core::Math::Vec3f maxValues) = 0;
+
+// TRANSFORMATION
+virtual void setViewParameters(float angle, float znear, float zfar) = 0;
+
+virtual void rotateCamera(Core::Math::Vec3f rotation) = 0;
+virtual void moveCamera(Core::Math::Vec3f direction) = 0;
+
+virtual void rotateObject(Core::Math::Vec3f rotation) = 0;
+virtual void moveObject(Core::Math::Vec3f direction) = 0;
+
+virtual void resetCamera() = 0;
+virtual void resetObject() = 0;
 
 
-    virtual void setClipVolume(Core::Math::Vec3f minValues, Core::Math::Vec3f maxValues) = 0;
 
-    virtual void setCompositeMode(ECompositeDisplay displaymode) = 0;
 
-    virtual void setRenderMode(ERenderMode mode) = 0;
 
-    virtual void set1DTransferFunction(std::vector<Core::Math::Vec4f> data) = 0;
-
-    virtual Core::Math::Vec4f readVolumePosition(Core::Math::Vec2ui v) = 0;
-
-    virtual void setClearViewEnabled(bool b) = 0;
-
-    virtual void setClearViewRadius(float f) = 0;
-
-    // virtual State getRenderState() = 0;
-
-    // virtual void setRenderState(State renderState) = 0;
-
-    virtual void switchPagingStrategy(MissingBrickStrategy brickStrategy) = 0;
 
