cc5cbabb5c4fa119eca0d19a3adf4f68d050bbd2
David McCann <mccann.dmc@gmail.com>
2016-03-24 20:33:41 +0000

    DummyRenderer derived from AbstractRenderer instead of IRenderer

--- src/processing-base/DummyRenderer.cpp
+++ src/processing-base/DummyRenderer.cpp
@@ -4,13 +4,4 @@
 using namespace trinity;
 
 DummyRenderer::DummyRenderer(std::shared_ptr<VisStream> stream, std::unique_ptr<IIO> ioSession)
-    : IRenderer(stream, std::move(ioSession)) {}
-
-void DummyRenderer::setIsoValue(float isoValue) {
-    LINFO("(p) iso value set to " + std::to_string(isoValue));
-    m_isoValue = isoValue;
-}
-
-void DummyRenderer::zoomCamera(float f){
-    LINFO("(p) camera zoom: " + std::to_string(f));
-}
+    : AbstractRenderer(stream, std::move(ioSession)) {}
--- src/processing-base/DummyRenderer.h
+++ src/processing-base/DummyRenderer.h
@@ -1,27 +1,17 @@
 #pragma once
 #include <memory>
 
-#include "common/IRenderer.h"
+#include "processing-base/AbstractRenderer.h"
 
 
 namespace trinity {
-class DummyRenderer : public IRenderer {
+class DummyRenderer : public AbstractRenderer {
 public:
     DummyRenderer(std::shared_ptr<VisStream> stream, std::unique_ptr<IIO> ioSession);
 
-    void setRenderMode(ERenderMode mode) override {}
-    bool supportsRenderMode(ERenderMode mode) override { return true; }
-
-    void setActiveModality(uint64_t modality) override {}
-    uint64_t getActiveModality() const override { return 0; }
-
-    void setIsoValue(float) override;
-    void initContext() override{};
-    void deleteContext() override{};
-
-    // unit test purposes
-    float getIsoValue() const { return m_isoValue; };
-    void zoomCamera(float f) override;
+    void initContext() override {};
+    void deleteContext() override {};
+    void paintInternal(PaintLevel paintlevel) override {}
 
 private:
     float m_isoValue;
