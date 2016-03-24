1b787118b8f7aed1fb88dfda414a4d9e8a6b3181
David McCann <mccann.dmc@gmail.com>
2016-03-24 20:17:06 +0000

    fixed templates

--- scripts/template/ProcessingCommandsHandlerTemplate.h
+++ scripts/template/ProcessingCommandsHandlerTemplate.h
@@ -1,9 +1,10 @@
 class {CommandName}Hdl : public ICommandHandler {
 public:
-    {CommandName}Hdl(const {CommandName}Request& request);
+    {CommandName}Hdl(const {CommandName}Request&request, RenderSession* session);
 
-    std::unique_ptr<commands::Reply> execute() override;
+    std::unique_ptr<Reply> execute() override;
 
 private:
     {CommandName}Request m_request;
+	RenderSession* m_session;
 };
