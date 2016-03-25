7b5c46c7377597743dbe69e20555b67c4a88dacc
Jens Krüger <jens.krueger@uni-due.de>
2016-03-25 19:52:09 +0000

    add a note about a potential security issue, to be addressed in the future

--- src/io-base/UVFListData.cpp
+++ src/io-base/UVFListData.cpp
@@ -6,7 +6,19 @@
 #include "silverbullet/io/FileTools.h"
 #include "silverbullet/base/StringTools.h"
 
-#define UVFDataRoot std::string("UVFData")
+// TODO Fix this:
+// Important Note:
+// currently we use the actual path of the file with a prefix (see below)
+// as dataset ID, however, this is a huge security issue as a potential
+// attacker can send just about any directory with the prefix and then
+// browse that directory and open the datasets there. One simple fix of
+// this issue would be to call the binary with chroot restricting it to a
+// selected dataset directory, a better solution would be to check the
+// the given path if it's within the intended directory (but that may be
+// nontrivial)
+
+#define UVFDataPrefix std::string("UVFData")
+#define UVFSearchRoot std::string(".")
 
 
 // compile this lister with the VALIDATE_FILES to check a
@@ -21,17 +33,17 @@ using namespace Core::IO::FileTools;
 using namespace Core::StringTools;
 
 bool UVFListData::stripListerID(std::string& id) const {
-  if (id.find(UVFDataRoot+"@") != 0) {
+  if (id.find(UVFDataPrefix+"@") != 0) {
 
-    if (id.find(UVFDataRoot) != 0)
+    if (id == UVFDataPrefix)
       return false;
     else {
-      id = id.substr(UVFDataRoot.length());
+      id = UVFSearchRoot;
       return true;
     }
   
   } else {
-    id = id.substr(UVFDataRoot.length());
+    id = id.substr(UVFDataPrefix.length());
     return true;
   }
 }
@@ -60,7 +72,7 @@ std::vector<IOData> UVFListData::listData(const std::string& dirID) const {
     for (const std::string& name : names) {
       if (isDirectory(name)) {
         ioDataVec.push_back(IOData(getFilename(name),
-                                   UVFDataRoot+"@"+name,
+                                   UVFDataPrefix+"@"+name,
                                    IOData::DataType::Directory));
       } else {
         // TODO: replace the first param of the IOData constructor
@@ -68,7 +80,7 @@ std::vector<IOData> UVFListData::listData(const std::string& dirID) const {
         // uvf-file
         if (ToLowerCase(getExt(path)) == "uvf")
           ioDataVec.push_back(IOData(getFilename(name),
-                                     UVFDataRoot+"@"+name,
+                                     UVFDataPrefix+"@"+name,
                                      IOData::DataType::Dataset));
       }
     }
