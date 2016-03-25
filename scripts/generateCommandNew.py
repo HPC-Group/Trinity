d73a4718d605b69d84d525ed6b44e377e28e0750
David McCann <mccann.dmc@gmail.com>
2016-03-25 19:55:17 +0000

    generate ctors and member lists

--- scripts/generateCommandNew.py
+++ scripts/generateCommandNew.py
@@ -26,6 +26,11 @@ templateInfosProc = [
 		"source":"../src/processing-base/ProcessingCommandsHandler.cpp",
 		"template":"ProcessingCommandsHandlerTemplate.cpp",
 		"marker":"#undef PYTHON_MAGIC"
+	},
+	{
+		"source":"../src/commands/ProcessingCommands.h",
+		"template":"ProcessingCommandsTemplate.h",
+		"marker":"#undef PYTHON_MAGIC"
 	}
 ]
 
@@ -38,6 +43,11 @@ templateInfosIO = [
 	{
 		"source":"../src/io-base/IOCommandsHandler.cpp",
 		"template":"IOCommandsHandlerTemplate.cpp",
+		"marker":"#undef PYTHON_MAGIC"
+	},
+	{
+		"source":"../src/commands/IOCommands.h",
+		"template":"IOCommandsTemplate.h",
 		"marker":"#undef PYTHON_MAGIC"
 	}
 ]
@@ -83,13 +93,51 @@ def tokenizeTemplate(template):
 			result.append(Token(TokenType.text, token))
 	return result
 
+def crQualify(type, name):
+	if type in ["int", "int8_t", "uint8_t", "int16_t", "uint16_t", "int32_t",
+				"uint32_t", "int64_t", "uint64_t", "bool", "float", "double"]:
+		return type + " " + name
+	else:
+		return "const " + type + "& " + name
+	
+def makeArgumentList(params):
+	items = []
+	for i in xrange(0, len(params), 2):
+		type = params[i]
+		name = params[i + 1]
+		items.append(crQualify(type, name))
+	return ", ".join(items)
+
+def makeMemberList(params):
+	items = []
+	for i in xrange(0, len(params), 2):
+		type = params[i]
+		name = params[i + 1]
+		items.append("\t\t" + type + " m_" + name + ";")
+	return "\tprivate:\n" + "\n".join(items)
+	
 def expandVariable(variable, input):
 	if variable == "VclType":
 		return input.commandName
+	elif variable == "CommandNameCmd":
+		return input.commandName + "Cmd"
 	elif variable == "CommandNameHdl":
 		return input.commandName + "Hdl"
 	elif variable == "CommandNameRequest":
 		return input.commandName + "Request"	
+	elif variable == "RequestCtor":
+		return "Request(" + makeArgumentList(input.params) + ");"
+	elif variable == "RequestMembers":
+		return makeMemberList(input.params)
+	elif variable == "ReplyCtor":
+		return "Request(" + makeArgumentList(input.ret) + ");"
+	elif variable == "ReplyMembers":
+		return makeMemberList(input.ret)
+	elif variable == "CommandNameReply":
+		return input.commandName + "Reply"
+	else:
+		raise Exception("Unknown variable " + variable)
+		
 		
 def process(templateInfos, input):
 	for templateInfo in templateInfos:
@@ -105,9 +153,11 @@ def process(templateInfos, input):
 		insertCodeInSource(templateInfo["source"], code, marker)
 
 class Input:
-	def __init__(self, commandName, hasReply):
+	def __init__(self, commandName, hasReply, params, ret):
 		self.commandName = commandName
 		self.hasReply = hasReply
+		self.params = params
+		self.ret = ret
 		
 def main():
 	global templateInfosAll
@@ -122,7 +172,7 @@ def main():
 	parser.add_argument('--parameters', nargs='*', help="all parameters of the command ('please specify all parameters successive, e.g., int x float y double z')")
 	args = parser.parse_args()
 
-	input = Input(args.name, args.hasReply)
+	input = Input(args.name, args.hasReply, args.parameters, [args.returnType, "result"])
 	if args.type == "proc":
 		templateInfos = templateInfosAll + templateInfosProc
 	elif args.type == "io":
--- scripts/template/ProcessingCommandsTemplate.h
+++ scripts/template/ProcessingCommandsTemplate.h
@@ -1,33 +1,37 @@
-struct {CommandName}Cmd {
+struct {{CommandNameCmd}} {
     static VclType Type;
 
     class RequestParams : public SerializableTemplate<RequestParams> {
     public:
         RequestParams() = default;
-
+		{{RequestCtor}}
+		
         void serialize(ISerialWriter& writer) const override;
         void deserialize(const ISerialReader& reader) override;
 
         std::string toString() const;
         bool equals(const RequestParams& other) const;
-    };
+{{RequestMembers}}
+	};
 
     class ReplyParams : public SerializableTemplate<ReplyParams> {
     public:
         ReplyParams() = default;
+		{{ReplyCtor}}
 
         void serialize(ISerialWriter& writer) const override;
         void deserialize(const ISerialReader& reader) override;
 
         std::string toString() const;
         bool equals(const ReplyParams& other) const;
-    };
+{{ReplyMembers}}
+	};
 };
 
-bool operator==(const {CommandName}Cmd::RequestParams& lhs, const {CommandName}Cmd::RequestParams& rhs);
-bool operator==(const {CommandName}Cmd::ReplyParams& lhs, const {CommandName}Cmd::ReplyParams& rhs);
-std::ostream& operator<<(std::ostream& os, const {CommandName}Cmd::RequestParams& obj);
-std::ostream& operator<<(std::ostream& os, const {CommandName}Cmd::ReplyParams& obj);
+bool operator==(const {{CommandNameCmd}}::RequestParams& lhs, const {{CommandNameCmd}}::RequestParams& rhs);
+bool operator==(const {{CommandNameCmd}}::ReplyParams& lhs, const {{CommandNameCmd}}::ReplyParams& rhs);
+std::ostream& operator<<(std::ostream& os, const {{CommandNameCmd}}::RequestParams& obj);
+std::ostream& operator<<(std::ostream& os, const {{CommandNameCmd}}::ReplyParams& obj);
 
-using {CommandName}Request = RequestTemplate<{CommandName}Cmd>;
-using {CommandName}Reply = ReplyTemplate<{CommandName}Cmd>;
+using {{CommandNameRequest}} = RequestTemplate<{{CommandNameCmd}}>;
+using {{CommandNameReply}} = ReplyTemplate<{{CommandNameCmd}}>;
