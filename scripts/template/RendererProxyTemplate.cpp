9acb007a4e11b55e13e77f7a5e9953906d5ddb02
Philipp Bergmann <philipp.bergmann@stud.uni-due.de>
2016-03-25 06:19:40 +0000

    template bug fix

--- scripts/template/RendererProxyTemplate.cpp
+++ scripts/template/RendererProxyTemplate.cpp
@@ -1,5 +1,5 @@
 {return} RendererProxy::{CommandName_small}({Parameters}) {
-	{CommandName}Cmd::RequestParams requestParams({Parameter_Variables});
+	{CommandName}Cmd::RequestParams params({Parameter_Variables});
 	{CommandName}Request request(params, IDGenerator::nextID(), m_remoteSessionId);
 	// TODO: not sure if code below is *always* generated correctly
 	m_inputChannel.sendRequest(request);
