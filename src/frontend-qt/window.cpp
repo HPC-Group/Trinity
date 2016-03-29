03ee7dc69e76266adf9bb051f1021f9cfa09555d
David McCann <mccann.dmc@gmail.com>
2016-03-29 20:28:47 +0000

    pan and set iso value with mouse in qt frontend

--- src/frontend-qt/window.cpp
+++ src/frontend-qt/window.cpp
@@ -1,7 +1,9 @@
 #include "window.h"
 
 #include "ui_window.h"
+#include <QMouseEvent>
 #include <QTimer>
+#include <QWheelEvent>
 
 #include <iostream>
 
@@ -16,58 +18,58 @@ using namespace mocca::net;
 static int reconnectInSec = 5;
 
 Window::Window(QWidget* parent)
-: QMainWindow(parent)
-, _initDone(false)
-, ui(new Ui::Window) {
-  ui->setupUi(this);
-  LINFO("Window created");
-  QTimer* timer = new QTimer(this);
-  connect(timer, SIGNAL(timeout()), this, SLOT(update()));
-  timer->start(10);
+    : QMainWindow(parent)
+    , _initDone(false)
+    , ui(new Ui::Window) {
+    ui->setupUi(this);
+    LINFO("Window created");
+    QTimer* timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(update()));
+    timer->start(10);
 }
 
 Window::~Window() {
-  delete ui;
+    delete ui;
 }
 
 /* OLD
  void Window::initRenderer() {
  trinity::StreamingParams params(_renderWidth, _renderHeight);
- 
+
  Endpoint endpointIO(ConnectionFactorySelector::tcpPrefixed(), "localhost", "6678");
- 
+
  // the file id will be available after implementing the listdata command
  int fileId = 0;
- _renderer = _processingNode->initRenderer(trinity::VclType::SimpleRenderer, fileId, endpointIO, params);
- 
+ m_renderer = _processingNode->initRenderer(trinity::VclType::SimpleRenderer, fileId, endpointIO, params);
+
  // sending commands
- _renderer->initContext();
+ m_renderer->initContext();
  std::this_thread::sleep_for(std::chrono::seconds(1));
  _initDone = true;
  }
- 
+
  void Window::on_IOconnectIP_clicked() {
  Endpoint endpointIO(ConnectionFactorySelector::tcpPrefixed(), "localhost", "6678");
- 
+
  _ioNode = std::unique_ptr<trinity::IONodeProxy>(new trinity::IONodeProxy(endpointIO));
  }
- 
+
  void Window::on_PRconnectIP_clicked() {
  Endpoint endpoint(ConnectionFactorySelector::tcpPrefixed(), "localhost", "8678");
- 
+
  _processingNode = std::unique_ptr<trinity::ProcessingNodeProxy>(new trinity::ProcessingNodeProxy(endpoint));
  LINFO("connected to processing node");
- 
+
  initRenderer();
  }
- 
+
  static float rot = 0.0f;
  void Window::update() {
  if (_initDone) {
- _renderer->setIsoValue(rot);
+ m_renderer->setIsoValue(rot);
  rot += 0.01f;
- 
- auto frameNullable = _renderer->getVisStream()->get();
+
+ auto frameNullable = m_renderer->getVisStream()->get();
  if (!frameNullable.isNull()) {
  auto frame = frameNullable.release();
  ui->openGLWidget->setData(_renderWidth, _renderHeight, frame.data());
@@ -77,98 +79,96 @@ Window::~Window() {
  }
  */
 
+void Window::printDataTree(const std::string& dataId, const std::string& indent) const {
+    try {
+        if (indent.empty()) {
+            LINFO("Listing " + dataId);
+        }
 
-void Window::printDataTree(const std::string& dataId,
-                           const std::string& indent) const {
-  try {
-    if (indent.empty()) {
-      LINFO("Listing " + dataId);
+        std::vector<trinity::IOData> list = _ioNode->listFiles(dataId);
+        for (auto e : list) {
+            if (e.getDataType() == trinity::IOData::DataType::Directory) {
+                LINFO(indent + "Directory:" + e.getName() + "\t (" + e.getFileId() + ")");
+                printDataTree(e.getFileId(), indent + "  ");
+            } else {
+                LINFO(indent + e.getName() + "\t (" + e.getFileId() + ")");
+            }
+        }
+    } catch (const trinity::TrinityError&) {
     }
-    
-    std::vector<trinity::IOData> list = _ioNode->listFiles(dataId);
-    for (auto e : list) {
-      if (e.getDataType() == trinity::IOData::DataType::Directory) {
-        LINFO(indent + "Directory:" + e.getName()+"\t ("+e.getFileId()+")");
-        printDataTree(e.getFileId(), indent+"  ");
-      } else {
-        LINFO(indent+e.getName()+"\t ("+e.getFileId()+")");
-      }
-    }
-  } catch (const trinity::TrinityError&) {
-    
-  }
 }
 
 void Window::initRenderer() {
-  _renderWidth = (ui->resx->text().toInt());
-  _renderHeight =(ui->resy->text().toInt());
-  trinity::StreamingParams params(_renderWidth, _renderHeight);
-  
-  Endpoint endpointIO(ConnectionFactorySelector::tcpPrefixed(),
-                      ui->IOaddressIPedit->text().toStdString(),
-                      ui->IOaddressPortedit->text().toStdString());
-  
-  // the file id will be available after implementing the listdata command
-  std::string fileId = "FractalData@3a"; // fixme: should be selectable from the gui
-  //std::string fileId = "UVFData@c60.uvf"; // fixme: should be selectable from the gui
-  
-  printDataTree("FractalData");
-  printDataTree("UVFData");
-  
-  
-  
-  try {
-    _renderer = _processingNode->initRenderer(trinity::VclType::SimpleRenderer,
-                                              fileId, endpointIO, params);
-   // _renderer->initContext();
-  } catch (const trinity::TrinityError&) {
-    LERROR("(qt) no connection to a renderer");
-  }
-  
-  std::this_thread::sleep_for(std::chrono::seconds(1));
-  _initDone = true;
+    _renderWidth = (ui->resx->text().toInt());
+    _renderHeight = (ui->resy->text().toInt());
+    trinity::StreamingParams params(_renderWidth, _renderHeight);
+
+    Endpoint endpointIO(ConnectionFactorySelector::tcpPrefixed(), ui->IOaddressIPedit->text().toStdString(),
+                        ui->IOaddressPortedit->text().toStdString());
+
+    // the file id will be available after implementing the listdata command
+    std::string fileId = "FractalData@3a"; // fixme: should be selectable from the gui
+    // std::string fileId = "UVFData@c60.uvf"; // fixme: should be selectable from the gui
+
+    printDataTree("FractalData");
+    printDataTree("UVFData");
+
+    try {
+        m_renderer = _processingNode->initRenderer(trinity::VclType::SimpleRenderer, fileId, endpointIO, params);
+        // m_renderer->initContext();
+    } catch (const trinity::TrinityError&) {
+        LERROR("(qt) no connection to a renderer");
+    }
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    _initDone = true;
 }
 
 void Window::on_IOconnectIP_clicked() {
-  Endpoint endpointIO(ConnectionFactorySelector::tcpPrefixed(),
-                      ui->IOaddressIPedit->text().toStdString(),
-                      ui->IOaddressPortedit->text().toStdString());
-  _ioNode = std::unique_ptr<trinity::IONodeProxy>(new trinity::IONodeProxy(endpointIO));
+    Endpoint endpointIO(ConnectionFactorySelector::tcpPrefixed(), ui->IOaddressIPedit->text().toStdString(),
+                        ui->IOaddressPortedit->text().toStdString());
+    _ioNode = std::unique_ptr<trinity::IONodeProxy>(new trinity::IONodeProxy(endpointIO));
 }
 
 void Window::on_PRconnectIP_clicked() {
-  Endpoint endpoint(ConnectionFactorySelector::tcpPrefixed(),
-                    ui->PRaddressIPedit->text().toStdString(),
-                    ui->PRaddressPortedit->text().toStdString());
-  
-  _processingNode = std::unique_ptr<trinity::ProcessingNodeProxy>(new trinity::ProcessingNodeProxy(endpoint));
-  LINFO("connected to processing node");
-  initRenderer();
+    Endpoint endpoint(ConnectionFactorySelector::tcpPrefixed(), ui->PRaddressIPedit->text().toStdString(),
+                      ui->PRaddressPortedit->text().toStdString());
+
+    _processingNode = std::unique_ptr<trinity::ProcessingNodeProxy>(new trinity::ProcessingNodeProxy(endpoint));
+    LINFO("connected to processing node");
+    initRenderer();
 }
 
-
 void Window::update() {
-  static float rot = 0.0f;
-  
-  if (_initDone && _renderer) {
-    
-    const bool somethingToDo = true;  // in this demo there is always something to do
-    
-    if (somethingToDo) {
-      _renderer->setIsoValue(0, rot);
-      rot += 0.01f;
-    } else {
-      _renderer->proceedRendering();
+    static float rot = 0.0f;
+    if (_initDone && m_renderer) {
+        auto frameNullable = m_renderer->getVisStream()->get();
+        if (!frameNullable.isNull()) {
+            auto frame = frameNullable.release();
+            ui->openGLWidget->setData(_renderWidth, _renderHeight, frame.data());
+            ui->openGLWidget->repaint();
+        }
     }
-    
-    auto frameNullable = _renderer->getVisStream()->get();
-    if (!frameNullable.isNull()) {
-      auto frame = frameNullable.release();
-      ui->openGLWidget->setData(_renderWidth, _renderHeight, frame.data());
-      ui->openGLWidget->repaint();
-      
-    }
-  }
 }
 
 void Window::repaint() {}
+
+void Window::wheelEvent(QWheelEvent* event) {
+    static float isoValue = 0;
+    isoValue += event->delta() / 2400.f;
+    m_renderer->setIsoValue(0, isoValue);
+}
+
+void Window::mousePressEvent(QMouseEvent* event) {
+    m_mousePosX = event->localPos().x();
+    m_mousePosY = event->localPos().y();
+}
+
+void Window::mouseMoveEvent(QMouseEvent* event) {
+    float deltaX = (m_mousePosX - event->localPos().x()) / 200.f;
+    float deltaY = (m_mousePosY - event->localPos().y()) / 200.f;
+    Core::Math::Vec3f vec(-deltaX, deltaY, .0f);
+    m_renderer->moveCamera(vec);
+    m_mousePosX = event->localPos().x();
+    m_mousePosY = event->localPos().y();
+}
--- src/frontend-qt/window.h
+++ src/frontend-qt/window.h
@@ -12,41 +12,44 @@
 #include "frontend-base/ProcessingNodeProxy.h"
 
 namespace Ui {
-  class Window;
+class Window;
 }
 
 class Window : public QMainWindow {
-  Q_OBJECT
-  
+    Q_OBJECT
+
 public:
-  explicit Window(QWidget* parent = 0);
-  ~Window();
-  void repaint();
-  
+    explicit Window(QWidget* parent = 0);
+    ~Window();
+    void repaint();
+
 private:
-  void initRenderer();
-  
-  private slots:
-  void on_IOconnectIP_clicked();
-  
-  void update();
-  
-  void on_PRconnectIP_clicked();
-  
+    void initRenderer();
+
+private slots:
+    void on_IOconnectIP_clicked();
+    void on_PRconnectIP_clicked();
+
+    void update();
+    void wheelEvent(QWheelEvent* event) override;
+    void mousePressEvent(QMouseEvent* event) override;
+    void mouseMoveEvent(QMouseEvent* event) override;
+
 private:
-  Ui::Window* ui;
-  uint32_t _renderWidth;
-  uint32_t _renderHeight;
-  
-  std::unique_ptr<trinity::ProcessingNodeProxy> _processingNode;
-  std::unique_ptr<trinity::IONodeProxy> _ioNode;
-  
-  std::unique_ptr<trinity::RendererProxy> _renderer;
-  bool _initDone;
-  
-  void printDataTree(const std::string& dataId,
-                     const std::string& indent="") const;
-  
+    Ui::Window* ui;
+    uint32_t _renderWidth;
+    uint32_t _renderHeight;
+
+    int m_mousePosX;
+    int m_mousePosY;
+
+    std::unique_ptr<trinity::ProcessingNodeProxy> _processingNode;
+    std::unique_ptr<trinity::IONodeProxy> _ioNode;
+
+    std::unique_ptr<trinity::RendererProxy> m_renderer;
+    bool _initDone;
+
+    void printDataTree(const std::string& dataId, const std::string& indent = "") const;
 };
 
 #endif // WINDOW_H
