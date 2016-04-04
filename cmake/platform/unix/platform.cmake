7de809f8223689d43f8dbba8b1dc3e9d480d5e22
David McCann <mccann.dmc@gmail.com>
2016-04-04 20:19:37 +0000

    removed hard coded -ggdb compiler flag

--- cmake/platform/unix/platform.cmake
+++ cmake/platform/unix/platform.cmake
@@ -1,2 +1,2 @@
 find_package(Threads)
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x -ggdb -fPIC -msse -msse2 -msse3 -pthread")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x -fPIC -msse -msse2 -msse3 -pthread")
