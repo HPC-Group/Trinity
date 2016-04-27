8f4e2a8c5a16944949bd39b0e3605b4176efc75f
David McCann <mccann.dmc@gmail.com>
2016-04-27 17:18:47 +0000

    removed fixme; embedded trinity icon in resources

--- src/frontend-qt/IODataModel.cpp
+++ src/frontend-qt/IODataModel.cpp
@@ -6,21 +6,20 @@ IODataModel::IODataModel(const trinity::IONodeProxy& ioNode, QObject* parent)
     auto rootNames = ioNode.getRoots();
     for (const auto& rootName : rootNames) {
         QStandardItem* item = new QStandardItem(QString::fromStdString(rootName));
+        item->setEditable(false);
         rootItem->appendRow(item);
         createRecursive(rootName, item);
     }
 }
 
 void IODataModel::createRecursive(const std::string& dataID, QStandardItem* item) {
-    try {
-        auto ioItems = m_ioNode.listFiles(dataID);
-        for (const auto& ioItem : ioItems) {
-            QStandardItem* newItem = new QStandardItem(QString::fromStdString(ioItem.getName()));
-            QVariant data = QVariant::fromValue(new trinity::IOData(ioItem));
-            newItem->setData(data);
-            item->appendRow(newItem);
-            createRecursive(ioItem.getFileId(), newItem);
-        }
-    } catch (const trinity::TrinityError&) {
-    } // fixme: an empty list would be nicer than an exception
+    auto ioItems = m_ioNode.listFiles(dataID);
+    for (const auto& ioItem : ioItems) {
+        QStandardItem* newItem = new QStandardItem(QString::fromStdString(ioItem.getName()));
+        newItem->setEditable(false);
+        QVariant data = QVariant::fromValue(new trinity::IOData(ioItem));
+        newItem->setData(data);
+        item->appendRow(newItem);
+        createRecursive(ioItem.getFileId(), newItem);
+    }
 }
